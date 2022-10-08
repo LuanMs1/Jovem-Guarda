ca838fc4192bd18c1380d7c87df08dd5071bb133
Felipe Barbosa <felipefb2@hotmail.com>
2022-10-08 20:04:55 -0300

    css files organized and disk page created

--- public/modules/register.js
+++ public/modules/register.js
@@ -4,54 +4,95 @@ export function register(evtBack, evtJoin, evtConfirmation) {
     app.innerHTML = `
       <div id="container-center-register">
         <a href="#">
-            <img class="link" src="./assets/images/jovemGuarda.png">
+            <img id="logo-register"class="link" src="./assets/images/jovemGuarda.png">
         </a>
-        <form action="" method="POST">
-            <span class="title">
+        <form id="form-register">
+            <span class="title-register">
                 CADASTRO
             </span>
             <div>
-              <input id="name" name="name" type="text" placeholder="NOME">
+              <input id="name-register" type="text" placeholder="NOME">
             </div>
             <div>
-                <input id="email" name="email" type="text" placeholder="EMAIL">
+                <input id="email-register" type="text" placeholder="EMAIL">
             </div>
             <div>
-                <input id="password" name="password" type="password" placeholder="SENHA">
+                <input id="password-register" type="password" placeholder="SENHA">
             </div>
             <div>
-                <input type="password" placeholder="CONFIRME SUA SENHA">
+                <input id="confirm-password-register" type="password" placeholder="CONFIRME SUA SENHA">
             </div>
+            <p id="message-error-register"></p>
             <div>
-                <input class="link" onsubmit="return false;" id="btn-submit" type="submit" value="CADASTRAR">
+                <input id="btn-submit-register"class="link" type="button" value="CADASTRAR">
             </div>
         </form>
         <span id="create-acount">
-          <a href="#" class="link">Voltar</a>
+          <a href="#" class="link">Entrar</a>
         </span>
       </div>
     `;
+
     registerService([evtBack, evtJoin, evtConfirmation]);
 }
 
 function registerService(evt) {
     const elements = document.querySelectorAll(".link");
 
+    const btnSubmit = document.querySelector("#btn-submit-register");
+
+    // btnSubmit.addEventListener("click", function () {
+    //     const pass = document.querySelector("#password-register").value;
+    //     const confirmPass = document.querySelector(
+    //         "#confirm-password-register"
+    //     ).value;
+    //     const msgError = document.querySelector("#message-error-register");
+    //     const control = 0;
+
+    //     console.log(`pass=${pass}`);
+    //     console.log(`confirm=${confirmPass}`);
+    //     if (pass !== confirmPass) {
+    //         msgError.innerHTML = "As senhas inseridas são diferentes";
+    //         control = 1;
+    //     } else {
+    //         console.log("OK!");
+    //         registerService([evtBack, evtJoin, evtConfirmation]);
+    //     }
+    // });
+
     for (let i = 0; i < elements.length; i++) {
         elements[i].onclick = () => {
             if (evt[i].detail.name == "/confirmation") {
-                registerUser();
+                const pass = document.querySelector("#password-register").value;
+                const confirmPass = document.querySelector(
+                    "#confirm-password-register"
+                ).value;
+                const msgError = document.querySelector(
+                    "#message-error-register"
+                );
+                const control = 0;
+
+                console.log(`pass=${pass}`);
+                console.log(`confirm=${confirmPass}`);
+                if (pass !== confirmPass) {
+                    msgError.innerHTML = "As senhas inseridas são diferentes";
+                    control = 1;
+                } else {
+                    registerUser();
+                    window.dispatchEvent(evt[i]);
+                }
+            } else {
+                window.dispatchEvent(evt[i]);
             }
-            window.dispatchEvent(evt[i]);
         };
     }
     document.title = "Register";
 }
 
 function registerUser() {
-    const name = document.getElementById("name").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
+    const name = document.querySelector("#name-register").value;
+    const email = document.querySelector("#email-register").value;
+    const password = document.querySelector("#password-register").value;
 
     const data = { name: name, email: email, password: password };
 
