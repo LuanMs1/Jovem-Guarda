e5166ca2cbcbd3b98d21521cd216a65734ae40fd
Luan <luan.mesquita.sobrinho@gmail.com>
2022-10-11 15:27:11 -0300

    fix: all disc route

--- src/controller/discs.js
+++ src/controller/discs.js
@@ -139,6 +139,18 @@ const deleteDisc = async (req, res) => {
         return res.status(500).json({message: err});
     }
 }
+const getAllDiscs = async (req,res) => {
+    const offset = req.params.offset;
+    try{
+        const discRes = await services.getAllDiscs(offset);
+        if (discRes.error) throw discRes.error;
+
+        res.status(200).json(discRes.result);
+    }catch(err){
+        
+        return res.status(500).json({message: err});
+    }
+}
 
 module.exports = {
     postDisc, 
@@ -146,5 +158,6 @@ module.exports = {
     updateDisc,
     deleteDisc,
     filter,
-    getUserDisc
+    getUserDisc,
+    getAllDiscs
 };
