d620cd566031f42ce2de3a83a08b8cbcef5765cc
Luan <luan.mesquita.sobrinho@gmail.com>
2022-10-11 11:55:31 -0300

    feat: https connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const https = require('https');
+// const privateKey = fs.readFileSync('../certificado/certificado.key');
+// const certificate= fs.readFileSync('../certificado/certificado.cert');
+
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate
+// };
 
 const app = express();
 
@@ -9,6 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 
 require("./router/index")(app);
 
+// const httpsServer = https.createServer(credentials, app);
 app.use(express.static("public"));
 
 app.listen(8000);
+// httpsServer.listen(443);
