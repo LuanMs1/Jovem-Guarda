703c716969c46583db5e63570261343cc2e671d7
yanvictorsn <yanvictosn@gmail.com>
2022-10-13 10:21:07 -0300

    feat: adding delete individual disc

--- public/modules/individualDisc.js
+++ public/modules/individualDisc.js
@@ -40,18 +40,24 @@ export function individualDisc(
 </section>
 <section id="description-disc-individual">
     <div id="description-disc-left">
-        <span class="infos-titles" id="album-type-individual">Tipo do Album: </span>
-        <span class="infos-titles" id="vinyl-type-individual" >Tipo do Vinil:</span>
-        <span class="infos-titles" id="length-individual">Duração:</span>
-        <span class="infos-titles" id="status-individual">Situação:</span>
-        <span class="infos-titles" id="owner-individual">Proprietário: </span>
+        <label for="">Tipo do Album: <span class="infos-titles" id="album-type-individual"></span></label>
+        
+        <label for="">Tipo do Vinil:<span class="infos-titles" id="vinyl-type-individual" ></span></label>
+        
+        <label for="">Duração:<span class="infos-titles" id="length-individual"></span></label>
+        
+        <label for="">Situação:<span class="infos-titles" id="status-individual"></span></label>
+        
+        <label for="">Proprietário:<span class="infos-titles" id="owner-individual"> </span></label>
+        
     </div>
     <div id="description-line-individual"></div>
     <div id="description-disc-right-individual">
-        <span id="description-individual">Descrição:</span>
+        <label for="">Descrição:<span id="description-individual"></span></label>
     </div>
 </section>
-<button id="btn-trade">TROCAR</button>
+<p id="msg-error-individual" ></p>
+<button id="btn-trade">DELETAR</button>
 <section id="musics-disc">
 
 </section>
@@ -144,17 +150,20 @@ function service(evt) {
     document.title = "Disco individual";
 }
 
+
 async function createIndividualDisc(id) {
     const res = await fetch(`/disc/one/${id}`, {
         method: "GET",
     });
+    
+    discID = id;
 
     const individualDisc = await res.json();
 
     const discTitle = document.getElementById("disc-title");
     discTitle.innerHTML = `${individualDisc[0].album}`;
 
-    const discArtist = document.getElementById("disc-title");
+    const discArtist = document.getElementById("disc-artist");
     discArtist.innerHTML = `${individualDisc[0].artist}`;
 
     const discYear = document.getElementById("disc-year");
@@ -214,26 +223,26 @@ async function createIndividualDisc(id) {
     const albumTypeIndividual = document.getElementById(
         "album-type-individual"
     );
-    albumTypeIndividual.innerHTML = `Tipo do Album: ${typeAlbumDiscSwitch}`;
+    albumTypeIndividual.innerHTML = ` ${typeAlbumDiscSwitch}`;
 
     const vinylTypeIndividual = document.getElementById(
         "vinyl-type-individual"
     );
-    vinylTypeIndividual.innerHTML = `Tipo do Vinil: ${typeVinylDiscSwitch}`;
+    vinylTypeIndividual.innerHTML = ` ${typeVinylDiscSwitch}`;
 
     const lengthIndividual = document.getElementById("length-individual");
-    lengthIndividual.innerHTML = `Duração: ${individualDisc[0].length}`;
+    lengthIndividual.innerHTML = ` ${individualDisc[0].length}`;
 
     const statusIndividual = document.getElementById("status-individual");
-    statusIndividual.innerHTML = `Situação: ${conditionDisc}`;
+    statusIndividual.innerHTML = ` ${conditionDisc}`;
 
     const ownerIndividual = document.getElementById("owner-individual");
-    ownerIndividual.innerHTML = `Proprietário: ${individualDisc[0].name}`;
+    ownerIndividual.innerHTML = ` ${individualDisc[0].name}`;
 
     const descriptionIndividual = document.getElementById(
         "description-individual"
     );
-    descriptionIndividual.innerHTML = `Descrição: ${individualDisc[0].disc_description}`;
+    descriptionIndividual.innerHTML = ` ${individualDisc[0].disc_description}`;
 
     const imgsDiscUpload = document.getElementsByClassName("imgs-disc-user");
 
@@ -246,4 +255,40 @@ async function createIndividualDisc(id) {
         imgDiscUploadCreate.className = "img-disc-upload-create";
         imgsDiscUpload[y].appendChild(imgDiscUploadCreate);
     }
+
+    const deleteButton = document.getElementById("btn-trade");
+    deleteButton.addEventListener("click", deleteIndividualDisc);
+
+}
+
+
+let discID = null;
+
+async function deleteIndividualDisc() {
+    const res = await fetch(`/user/disc/${discID}`, {
+        method: "DELETE",
+    });
+    const deleted = res.json()
+
+    document.getElementById("disc-title").style.textDecoration = "line-through"
+    document.getElementById("disc-artist").style.textDecoration = "line-through"
+    document.getElementById("disc-year").style.textDecoration = "line-through"
+    document.getElementById("disc-img").style.opacity = "0.3"
+    document.getElementById("album-type-individual").style.textDecoration = "line-through"
+    document.getElementById("vinyl-type-individual").style.textDecoration = "line-through"
+    document.getElementById("length-individual").style.textDecoration = "line-through"
+    document.getElementById("status-individual").style.textDecoration = "line-through"
+    document.getElementById("owner-individual").style.textDecoration = "line-through"
+    document.getElementById(
+        "description-individual"
+    ).style.textDecoration = "line-through"
+
+    const msgError = document.getElementById("msg-error-individual");
+        msgError.innerHTML = "Disco excluído com sucesso!"
+        
+        
+    // if (res.status == 200) {
+    
+    // }
+
 }
